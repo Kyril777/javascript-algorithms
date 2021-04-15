8bb97c827f4bbda81a66894fa77c2937035b0477
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-04-14 23:06:01 -0400

    Update erastosthenes.js

--- erastosthenes.js
+++ erastosthenes.js
@@ -15,3 +15,29 @@ const primes = number => {
   );
 };
 
+
+
+
+let primegenerator = (n) => {
+    let array = [], upperLimit = Math.sqrt(n), primes = [];
+
+    for (let i = 0; i < n; i++) {
+        array.push(true);
+    }
+
+    for (let i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (var j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
+
+    for (let i = 2; i < n; i++) {
+        if(array[i]) {
+            primes.push(i);
+        }
+    }
+
+    return primes;
+};
