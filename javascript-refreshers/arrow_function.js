d3891cc74a1084c24fe05386f1862d3f52bc22f3
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-09-06 23:41:39 -0400

    Update arrow_function.js

--- javascript-refreshers/arrow_function.js
+++ javascript-refreshers/arrow_function.js
@@ -20,3 +20,19 @@ class Person {
     }. 100) 
   } 
 } 
+
+/*
+Here, we have a simple person class that takes a name and the constructor. And sets this.name to name.  
+And then we have two functions. One is called printNameArrow. It uses an arrow function inside of a timeout. It will print out a person’s name after ‘Arrow: ’ following a 100 millisecond delay. 
+
+The printNameFunction will try to do the same thing except it uses the standard function syntax. 
+We will assign a new person and give it the name of class. We will then run both the printNameArrow and the printNameFunction. 
+
+You will then see that the printNameArrow produces the name while printNameFunction does not, 
+Normal standard function for JavaScript redefines the this keyword. But when you use an arrow function, this is not re-defined.  
+*/
+
+let person = new Person(‘Bob’) 
+person.printNameArrow() 
+person.printNameFunction() 
+console.log(this.name)  
