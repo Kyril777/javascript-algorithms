5bb8f971a9b33cf9a0f15f9e08a80db9819af76f
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-04-08 23:57:23 -0400

    Update stringreversal.js

--- stringreversal.js
+++ stringreversal.js
@@ -22,3 +22,15 @@ const reverseString = string {
   else
     return reverseString(str.substr(1)) + str.charAt(0);
 }
+
+
+const reverse = (s) => {
+  let o = '';
+  for (let i = s.length - 1; i >= 0; i--)
+    0 += s[i];
+  return o;
+}
+
+const reverseRecursive = (str) => {
+  return (s === '') ? '' : reverse(s.substr(1)) + s.charAt(0);
+}
