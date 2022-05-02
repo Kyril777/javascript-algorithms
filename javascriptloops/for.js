536bd1b0fa71ba037de142af19dc11f1fdbfa9f9
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-05-01 23:52:17 -0400

    Update and rename for.html to for.js

---
+++ javascriptloops/for.js
@@ -0,0 +1,16 @@
+/* Print digit from 1 - 5 in ascending order.  */
+for(let i=0; i<=5; i++){
+   console.log(i);
+}
+
+/* Print digit from 1 - 5 in descending order.  */
+for(let i=5; i>=0; i--){
+   console.log(i);
+}
+
+/* Looping through the values of an array. */
+const birds = ["canary", "osprey", "rook"];
+
+for (const x of birds) {
+  console.log(x)
+}
