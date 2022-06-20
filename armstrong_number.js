76dc6f8a69dc48a484964fabee72a6c86621a319
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-06-20 00:05:47 -0400

    Update armstrong_number.js

--- armstrong_number.js
+++ armstrong_number.js
@@ -1,25 +1,26 @@
-         function armstrong()
-         {
-             var num = ;
-             let sum = 0;
-             //store the number to check later
-             let tmp = num;
-             //Extract each digit from the number
-             while(num > 0){
-                  //retrieve the last digit
-                  let d = parseInt(num % 10);
-                  //find the cube
-                  d = d ** 3;
-                  sum = sum + d;
-                  //convert float to Integer
-                  num = parseInt(num / 10);
-             }
-             if(tmp === sum)
-             {
-                  alert("Armstrong number");
-             }
-             else
-             {
-                  alert("Not an armstrong number");
-             }
-         }
+const armstrong = ()=> {
+     var num = ;
+     let sum = 0;
+      
+     // Temporarily stores the number.
+     let temp = num;
+
+     // Take out each digit from the number.
+     while(num > 0){
+
+          // Obtain the last digit.
+          let d = parseInt(num % 10);
+          
+          // Find the cube.
+          d = d ** 3;
+          sum = sum + d;
+          // Convert float to an integer.
+          num = parseInt(num / 10);
+     }
+
+     if(tmp === sum){
+          return "Armstrong number";
+     } else {
+     return "Not an armstrong number";
+     }
+}
