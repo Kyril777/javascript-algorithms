6578daec1ae146fafe074c0a1cd76f452fa0544e
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-06-20 00:07:23 -0400

    Update armstrong_number.js

--- armstrong_number.js
+++ armstrong_number.js
@@ -1,3 +1,5 @@
+/* The Armstrong number of three digits is one integer such that the sum of the cubes of its digits is equal to the number itself. 371, for example, is an Armstrong number because 3**3 + 7**3 + 1**3 = 371. */
+
 const armstrong = ()=> {
      var num = ;
      let sum = 0;
