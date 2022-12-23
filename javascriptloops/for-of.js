dd095d11c9f7cb3b020fc66bd8b48faea9a0e7ac
Kyril Jardinico <Kyril.Jay@gmail.com>
2022-12-22 23:30:24 -0500

    Update for-of.js

--- javascriptloops/for-of.js
+++ javascriptloops/for-of.js
@@ -1,8 +1,17 @@
-/* The for of statement loops through the values of an iterable object. */
+/* The for-of statement loops through the values of an iterable object. */
 
-let state = "Indiana";
+/* Looping for a string.*/
+let state = "INDIANA";
 
 let text = "";
 for (let x of state) {
   text += x + "<br>";
+}
+
+/* Looping through an object. */
+const cars = ["New Jersey", "South Carolina", "Idaho"];
+
+let text = "";
+for (let x of states) {
+  text += x;
 }
