42a56a0eea89953caf28cfe2b0d2e117e0f80b14
Kyril Jardinico <Kyril.Jay@gmail.com>
2023-07-24 19:17:37 -0400

    Update displayTime.js
    Add a clearInterval() function.

--- javascript-time-programs/displayTime.js
+++ javascript-time-programs/displayTime.js
@@ -12,3 +12,30 @@ function displayTime() {
 
 // Display time every ten seconds.
 let display = setInterval(displayTime, 10000);
+
+
+// Revision of previous function only with a clear interval.
+let count = 0;
+let interval = setInterval(function(){
+
+    // Increase count by 1.
+    count += 1;
+
+    // Stop count after 5 returns.
+    if(count === 5){
+        clearInterval(interval);
+    }
+
+    // Display time. 
+    let dateTime= new Date();
+    let time = dateTime.toLocaleTimeString();
+    console.log(time);
+
+}, 2000);
+/*
+"7:15:54 PM"
+"7:15:56 PM"
+"7:15:58 PM"
+"7:16:00 PM"
+"7:16:02 PM"
+*/
